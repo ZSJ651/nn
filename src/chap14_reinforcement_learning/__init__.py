78eda7b672156b3fb4ca00c6e98f0ddd28a87dbb
ZSJ651 <3244097908@qq.com>
2025-06-03 14:41:12 +0800

    添加注释

--- src/chap14_reinforcement_learning/__init__.py
+++ src/chap14_reinforcement_learning/__init__.py
@@ -4,7 +4,7 @@ from gym.envs.registration import registry, register, make, spec
 # ----------------------------------------
 
 register(
-    id='Copy-v0',
+    id='Copy-v0',  # 环境唯一标识符
     entry_point='gym.envs.algorithmic:CopyEnv',
     max_episode_steps=200,
     reward_threshold=25.0,
